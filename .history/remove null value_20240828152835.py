55b0e891a3f7aeca20995c2075aac780f44a4664
ghaffar234 <bajwaghaffar269@gmail.com>
2024-08-28 15:42:09 +0500

    this is remove null value

---
+++ .history/remove null value_20240828152835.py
@@ -0,0 +1,4 @@
+#this programme for the remove the null value and put new value
+import pandas as pd
+var=pd.read_csv("C:\\Users\\Hp\\Downloads\\paractice sheet 1 null value.csv")
+print(var.fillna("Ghaffar"))
---
+++ remove null value.py
@@ -0,0 +1,12 @@
+#this programme for the remove the null value and put new value
+import pandas as pd
+var=pd.read_csv("C:\\Users\\Hp\\Downloads\\paractice sheet 1 null value.csv")
+#this function use as the fill value in null value
+#print(var.fillna("Ghaffar"))
+#this function is used for drop the null value row
+#print(var.dropna())
+#This function is use for fill the forward and backword filing
+#print(var.dropna(how="any"))
+#fill any value in the null position of sheet
+var.fillna(13,inplace=True)
+print(var)
